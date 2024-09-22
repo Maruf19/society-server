7da2072f90fd65f3c1f2d6825884bec8634f17ee
maruf19 <marufrony48@gmail.com>
2024-09-22 12:31:40 +0600

    updated

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ async function run() {
     const contactsCollection = database.collection("contacts");
     const schedulesCollection = database.collection("schedule");
     const achievementCollection = database.collection("achievement");
+    const youtubeCollection = database.collection("youtube");
+    const facebookCollection = database.collection("facebook");
+    const newsCollection = database.collection("news");
+    const aboutCollection = database.collection("about");
+    const leadersCollection = database.collection("leader")
+    const teamCollection = database.collection("team")
 
     // Send contact data
     app.post('/contact', async (req, res) => {
@@ -68,6 +74,100 @@ async function run() {
       const result = await achievementCollection.insertOne(achievement);
       res.send(result);
     });
+
+    // Get youtube data
+    app.get('/youtube', async (req, res) => {
+      try {
+        const cursor = youtubeCollection.find({});
+        const youtube= await cursor.toArray();
+        res.json(youtube);
+      } catch (err) {
+        res.status(500).send('Error fetching youtube');
+      }
+    });
+
+    // Post youtube data (admin route)
+    app.post("/youtube", async (req, res) => {
+      const youtube = req.body;
+      const result = await youtubeCollection.insertOne(youtube);
+      res.send(result);
+    });
+
+     // Get facebook data
+     app.get('/facebook', async (req, res) => {
+      try {
+        const cursor = facebookCollection.find({});
+        const facebook = await cursor.toArray();
+        res.json(facebook);
+      } catch (err) {
+        res.status(500).send('Error fetching Facebook data');
+      }
+    });
+    
+
+    // Post facebook data (admin route)
+    app.post("/facebook", async (req, res) => {
+      const facebook = req.body;
+      const result = await facebookCollection.insertOne(facebook);
+      res.send(result);
+    });
+
+     // Get news data
+     app.get('/news', async (req, res) => {
+      try {
+        const cursor = newsCollection.find({});
+        const news= await cursor.toArray();
+        res.json(news);
+      } catch (err) {
+        res.status(500).send('Error fetching news');
+      }
+    });
+
+    // Post news data (admin route)
+    app.post("/news", async (req, res) => {
+      const news = req.body;
+      const result = await newsCollection.insertOne(news);
+      res.send(result);
+    });
+
+     //About Get Data
+    app.get('/about', async (req, res) => {
+      try {
+        const cursor = aboutCollection.find({});
+        const About = await cursor.toArray();
+        res.json(About);
+      } catch (err) {
+        res.status(500).send('Error fetching About');
+      }
+    });
+
+    // Post about data (admin route)
+    app.post("/about", async (req, res) => {
+      const about = req.body;
+      const result = await aboutCollection.insertOne(about);
+      res.send(result);
+    });
+
+      //Leader Get Data
+      app.get('/leader', async (req, res) => {
+        try {
+          const cursor = leadersCollection.find({});
+          const Leader = await cursor.toArray();
+          res.json(Leader);
+        } catch (err) {
+          res.status(500).send('Error fetching About');
+        }
+      });
+  
+      // Post Leader data (admin route)
+      app.post("/about", async (req, res) => {
+        const Leader = req.body;
+        const result = await leadersCollection.insertOne(about);
+        res.send(result);
+      });
+
+      
+
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
